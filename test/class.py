e951b3f0eb18f2642484bd0ee7a1b82cce9c83ee
stella <70000city@gmail.com>
2021-09-24 18:47:51 +0900

    [study] 파이썬 기초문법 공부

---
+++ test/class.py
@@ -0,0 +1,42 @@
+class FourCal:
+    def __init__(self, first, second):
+        self.first = first
+        self.second = second
+
+    def setsata(self, first, second):
+        self.first  = first
+        self.second = second
+
+    def add(self):
+        result = self.first+ self.second
+        return result
+    def mul(self):
+        result = self.first * self.second
+        return result
+    def div(self):
+        result = self.first / self.second
+        return result
+    def sub(self):
+        result = self.first - self.second
+        return result
+
+class MoreFourCal(FourCal):
+    def pow(self):
+        result = self.first ** self.second
+        return result
+
+class SafeFourCal(FourCal):
+    def div(self):
+        if self.second == 0:
+            return 0
+        else:
+            return self.first / self.second
+
+
+a = FourCal(3, 4)
+b = MoreFourCal(4, 4)
+c = SafeFourCal(4, 0)
+print(c.div())
+# a.setsata(1,2)
+print(a.add())
+print(b.pow())
